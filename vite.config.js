ef0316f6970bc0137ffbbd1a5fd12b5f17ccb143
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2022-09-15 23:18:49 -0300

    add initial configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import tailwindcss from 'tailwindcss'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()]
+	plugins: [preact(), tailwindcss()],
 })
